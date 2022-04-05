ee2b7393d22331d4b711b95125c0da58e2c1e4e3
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2022-04-05 17:45:50 +0800

    Testing out the LCD.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,51 @@
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/env python3
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+from ST7789 import ST7789, BG_SPI_CS_FRONT
+from PIL import Image, ImageDraw
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+import random
+import time
+
+# Buttons
+BUTTON_A = 4
+BUTTON_B = 7
+BUTTON_X = 3
+BUTTON_Y = 0
+
+# Onboard RGB LED
+LED_R = 17
+LED_G = 27
+LED_B = 22
+
+# General
+SPI_PORT = 0
+SPI_CS = 10
+SPI_DC = 6
+BACKLIGHT = 5
+
+# Screen dimensions
+WIDTH = 240
+HEIGHT = 320
+
+buffer = Image.new("RGB", (WIDTH, HEIGHT))
+draw = ImageDraw.Draw(buffer)
+
+draw.rectangle((0, 0, 50, 50), (255, 0, 0))
+draw.rectangle((320-50, 0, 320, 50), (0, 255, 0))
+draw.rectangle((0, 240-50, 50, 240), (0, 0, 255))
+draw.rectangle((320-50, 240-50, 320, 240), (255, 255, 0))
+
+display = ST7789(
+    port=SPI_PORT,
+    cs=SPI_CS,
+    dc=SPI_DC,
+    backlight=BACKLIGHT,
+    width=WIDTH,
+    height=HEIGHT,
+    rotation=180,
+    spi_speed_hz=60 * 1000 * 1000
+)
+
+while True:
+    display.display(buffer)
+    time.sleep(1.0 / 60)
