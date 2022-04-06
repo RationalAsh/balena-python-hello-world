5c2089296c28bb9449d214e57b8b91368b06ac12
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2022-04-06 14:04:25 +0800

    Debugging drawing to the screen.

--- src/main.py
+++ src/main.py
@@ -33,13 +33,6 @@ BACKLIGHT = 24
 WIDTH = 240
 HEIGHT = 320
 
-buffer = Image.new("RGB", (WIDTH, HEIGHT))
-draw = ImageDraw.Draw(buffer)
-
-draw.rectangle((0, 0, 50, 50), (255, 0, 0))
-draw.rectangle((320-50, 0, 320, 50), (0, 255, 0))
-draw.rectangle((0, 240-50, 50, 240), (0, 0, 255))
-draw.rectangle((320-50, 240-50, 320, 240), (255, 255, 0))
 
 display = ST7789(
     port=SPI_PORT,
@@ -52,10 +45,25 @@ display = ST7789(
     spi_speed_hz=60 * 1000 * 1000
 )
 
-while True:
-    display.display(buffer)
-    print("Successfully drew to screen.")
-    time.sleep(1.0)
+
+if __name__ == '__main__':
+    while True:
+        buffer = Image.new("RGB", (WIDTH, HEIGHT))
+        draw = ImageDraw.Draw(buffer)
+
+        draw.rectangle((0, 0, 50, 50), (255, 0, 0))
+        draw.rectangle((320 - 50, 0, 320, 50), (0, 255, 0))
+        draw.rectangle((0, 240 - 50, 50, 240), (0, 0, 255))
+        draw.rectangle((320 - 50, 240 - 50, 320, 240), (255, 255, 0))
+
+        display.display(buffer)
+
+        time.sleep(1.0)
+
+# while True:
+#     display.display(buffer)
+#     print("Successfully drew to screen.")
+#     time.sleep(1.0)
 
 # if __name__ == '__main__':
 #     while True:
